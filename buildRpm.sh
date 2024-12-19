9bab1c16717c2f980e725b35bf6f71fd7052e02c
Ryan Doherty <tech@conical.org>
2024-12-18 21:03:33 -0500

    Update spec to latest tomcat 9 patch version and refine script to copy generated rpms to cwd

--- buildRpm.sh
+++ buildRpm.sh
@@ -1,38 +1,48 @@
-#!/bin/bash
+#!/bin/sh
+#################################################
+## Copied from Jenkins job performing this task
+#################################################
 
 export WORKSPACE=/tmp
 export BUILDDIR=build
-export source_dir=$(pwd)
-export package_name=tomcat-instance-framework
+export PKG_NAME=tomcat-instance-framework
 export PKG_VERSION=$(cat VERSION)
 
-rm    -rf ${WORKSPACE}/${BUILDDIR}
-mkdir -p  ${WORKSPACE}/${BUILDDIR}
-mkdir -p  ${WORKSPACE}/${BUILDDIR}/rpm-build/RPM/SRPM
+# purge any previous work directories and recreate
+rm -rf ${WORKSPACE}/${BUILDDIR}
+mkdir -p ${WORKSPACE}/${BUILDDIR}/rpm-build/RPM/SRPM
 
-cd ${source_dir}
+runtimeDir=$(pwd)
+sourceDir=$(dirname $(realpath $0))
+
+cd $sourceDir
+
 git log --date=short --pretty=format:"%h%x09%an%x09%ad%x09%s" > Changelog
 
 cd ${WORKSPACE}/${BUILDDIR}
-ln -s ${source_dir} ${package_name}-${PKG_VERSION}
-tar -czf ${WORKSPACE}/${BUILDDIR}/rpm-build/${package_name}-${PKG_VERSION}.tar.gz \
+ln -s ${sourceDir} ${PKG_NAME}-${PKG_VERSION}
+tar -czf ${WORKSPACE}/${BUILDDIR}/rpm-build/${PKG_NAME}-${PKG_VERSION}.tar.gz \
     -C ${WORKSPACE}/${BUILDDIR} \
     --exclude .git \
     --exclude .gitignore \
     --exclude ${BUILDDIR} \
-    -h ${package_name}-${PKG_VERSION}
+    -h ${PKG_NAME}-${PKG_VERSION}
+
 cd -
 
 rpmbuild \
--bb \
---define "_topdir ${WORKSPACE}/${BUILDDIR}/rpm-build" \
---define "_builddir %{_topdir}" \
---define "_rpmdir %{_topdir}/RPM" \
---define "_build_name_fmt %%{NAME}-%%{VERSION}-%%{RELEASE}.%%{ARCH}.rpm" \
---define "_srcrpmdir %{_topdir}/RPM" \
---define "_specdir %{_topdir}" \
---define "_sourcedir  %{_topdir}" \
---define "vendor EuPathDB" \
---define "pkg_version ${PKG_VERSION}" \
---define "debug_package %{nil}" \
-${source_dir}/tomcat-instance-framework.spec
+  -ba \
+  --define "_topdir ${WORKSPACE}/${BUILDDIR}/rpm-build" \
+  --define "_builddir %{_topdir}" \
+  --define "_rpmdir %{_topdir}/RPM" \
+  --define "_build_name_fmt %%{NAME}-%%{VERSION}-%%{RELEASE}.%%{ARCH}.rpm" \
+  --define "_srcrpmdir %{_topdir}/RPM" \
+  --define "_specdir %{_topdir}" \
+  --define "_sourcedir  %{_topdir}" \
+  --define "vendor EuPathDB" \
+  --define "pkg_version ${PKG_VERSION}" \
+  --define "debug_package %{nil}" \
+  ${sourceDir}/tomcat-instance-framework.spec
+
+# copy generated RPMs to the runtime directory
+cp ${WORKSPACE}/${BUILDDIR}/rpm-build/RPM/*.rpm $runtimeDir
